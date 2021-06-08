81c38f8ca6367a0307ca39e98dfa851c0bc37ccb
vinhqngo5 <vinhqngo5@gmail.com>
2021-06-08 18:35:01 +0700

    Add: changing velocity of bullet based on pressing [space] (desktop)

--- WebSrc/script.js
+++ WebSrc/script.js
@@ -1,11 +1,15 @@
-const shoot = (velocity = 10) => {
+const GAME = {
+	MAX_VELOCITY: 20,
+	DEFAULT_VELOCITY: 10,
+};
+
+const shoot = (velocity = GAME.DEFAULT_VELOCITY) => {
 	const bullet = document.createElement("a-sphere");
 	let pos = myCamera.getAttribute("position");
 	bullet.setAttribute("position", pos);
 	bullet.setAttribute("velocity", getDirection(myCamera, velocity));
 	bullet.setAttribute("dynamic-body", true);
 	bullet.setAttribute("radius", 0.2);
-	// bullet.setAttribute("src", "https://i.imgur.com/H8e3Vnu.png");
 	bullet.setAttribute("src", "https://i.imgur.com/H8e3Vnu.png");
 	myScene.appendChild(bullet);
 	bullet.addEventListener("collide", shootCollided);
@@ -15,13 +19,34 @@ const shootCollided = (event) => {
 	console.log("Hit something");
 };
 
-const shootInDesktop = (event) => {
-	if (event.which == 32) {
-		shoot();
-	}
-};
-const shootInMobile = (event) => {
-	shoot();
+const shootInDesktop = (el, callback) => {
+	let touchsurface = el,
+		elapsedTime,
+		startTime,
+		handlePress = callback || function (swipedir) {};
+
+	touchsurface.addEventListener(
+		"keydown",
+		function (e) {
+			if (e.repeat) {
+				return; // Event fired when press too long
+			}
+			startTime = new Date().getTime(); // record time when finger first makes contact with surface
+			e.preventDefault();
+		},
+		false
+	);
+
+	touchsurface.addEventListener(
+		"keyup",
+		function (e) {
+			elapsedTime = new Date().getTime() - startTime; // get time elapsed
+			console.log(new Date().getTime());
+			handlePress(e, Math.min(elapsedTime / 20, GAME.MAX_VELOCITY));
+			e.preventDefault();
+		},
+		false
+	);
 };
 
 function swipedetect(el, callback) {
@@ -36,7 +61,7 @@ function swipedetect(el, callback) {
 		allowedTime = 300, // maximum time allowed to travel that distance
 		elapsedTime,
 		startTime,
-		handleswipe = callback || function (swipedir) {};
+		handleswipe = callback || ((swipedir, velocity) => {});
 
 	touchsurface.addEventListener(
 		"touchstart",
@@ -80,15 +105,16 @@ function swipedetect(el, callback) {
 					swipedir = distY < 0 ? "up" : "down"; // if dist traveled is negative, it indicates up swipe
 				}
 			}
-			handleswipe(swipedir, Math.min(Math.abs(distY / 8), 20));
+			handleswipe(swipedir, Math.min(Math.abs(distY / 8), GAME.MAX_VELOCITY));
 			e.preventDefault();
 		},
 		false
 	);
 }
-document.addEventListener("keydown", shootInDesktop);
-// document.addEventListener("touchstart", shootInMobile);
+
+shootInDesktop(document, function (event, velocity) {
+	if (event.which == 32) shoot(velocity);
+});
 swipedetect(document, function (swipedir, velocity) {
-	console.log(Math.min(Math.abs(velocity), 50));
 	if (swipedir == "up") shoot(velocity);
 });
